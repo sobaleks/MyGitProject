3bf05c7805a3acdad164e598581ae0feb4a8d7ec
Alexandar <aleksandar.sobolevscky@yandex.by>
2022-09-28 21:25:42 +0300

    это код новой ветки 2

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello world!!!!!");
         System.out.println("Hello world!!!!!");
         System.out.println(" имитация разаработки");
+        System.out.println(" имитация разаработки");
+        System.out.println(" имитация разаработки");
     }
 }
